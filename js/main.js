5539063303c2d2994b3795f7faa3f11a31b21ff2
Simonov Roman <roman.viktorovich@outlook.com>
2022-12-09 17:51:02 +0300

    style pages and event btn

--- js/main.js
+++ js/main.js
@@ -8,8 +8,9 @@ const btn = document.body.querySelector('.heart__btn')
 const err = 'Геолокация не поддерживается этим браузером.'
 const temperature = document.querySelector('.heart__degreeCelsius')
 const city = document.querySelector('.heart__locations')
-
-
+const heart = document.body.querySelector('.heart')
+const input = document.createElement('input')
+console.log(heart)
 
 async function showPosition(position) {
 
@@ -29,53 +30,39 @@ async function showPosition(position) {
     let temperature = document.querySelector('.heart__degreeCelsius')
     let city = document.querySelector('.heart__locations')
 
-    // temperature.innerHTML =  (data.main.temp -32)   / 1.8
     temperature.innerHTML =  (data.main.temp - 273).toFixed(1) + '℃'
     city.innerHTML = data.name
 
-
-
-
 }
-/*font-family: 'Inter';
-font-style: normal;
-font-weight: 400;
-font-size: 20px;
-line-height: 36px;
-/* identical to box height, or 180% */
-
-
-
 
 function getLocation() {
         navigator.geolocation.getCurrentPosition(showPosition, err => {
             temperature.innerHTML= 'Ooops. Something went wrong.'
-            temperature.style.fontWeight = "400px"
-            temperature.style.fontSize = "20px"
-            temperature.style.lineHeight = "36px"
-
+            // temperature.style.fontWeight = "400px"
+            // temperature.style.fontSize = "20px"
+            // temperature.style.lineHeight = "36px"
             city.innerHTML = 'Error info'
-            city.style.opacity = '0.75'
-            city.style.fontWeight = "400px"
-            city.style.fontSize = "12px"
-            city.style.lineHeight = "15px"
-
-
-            btn.style.backgroundColor = 'white'
-            btn.style.width = '140px'
-            btn.style.height = '48px'
-            btn.style.borderRadius = '8px'
-            btn.style.backgroundColor = 'white'
-            btn.style.fontFamily = 'Inter'
-            btn.style.textDecoration = 'none'
-            btn.style.color = '#4C01E0'
-            btn.style.fontWeight = '400'
-            btn.style.fontSize = '20px'
-            btn.style.lineHeight = '36px'
-
-
-
+            // city.style.opacity = '0.75'
+            // city.style.fontWeight = "400px"
+            // city.style.fontSize = "12px"
+            // city.style.lineHeight = "15px"
+            btn.textContent = 'Try again'
         })
 }
 getLocation()
+
+
+
+function searchCity (){
+    /*<input id="search" type="text" placeholder="Type your city here">*/
+    btn.textContent = 'Find'
+    btn.className = 'btn__heart-err'
+    city.style.display = 'none'
+    temperature.style.display = 'none'
+    input.placeholder='Type your city here'
+    input.type='text'
+    heart.prepend(input)
+}
+
+btn.addEventListener('click', searchCity)
 
